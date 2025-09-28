7acfc4d41968e125a8ca99a6178e6156602c902c
Mayank <mayankchaurasiya66@gmail.com>
2025-09-28 12:56:38 +0530

    added pattern11

--- DSA-JAVA/Patterns.java
+++ DSA-JAVA/Patterns.java
@@ -29,12 +29,113 @@ public class Patterns {
     }
 
     public static void pattern4(int n) {
-        int count = 1;
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             for (int j = 0; j <= i; j++) {
-                System.out.print(count + " ");
+                System.out.print(i + " ");
             }
-            count++;
+            System.out.println();
+        }
+    }
+
+    public static void pattern5(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern6(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern7(int n) {
+        for (int i = 0; i < n; i++) {
+            // space
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            // star
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern8(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * n - (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern9(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * n - (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern10(int n) {
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            int stars = i;
+            if (i > n) {
+                stars = 2 * n - i;
+            }
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern11(int n) {
+        int start = 1;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                start = 1;
+            } else {
+                start = 0;
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(start);
+                start = 1 - start;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern12(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
             System.out.println();
         }
     }
@@ -44,7 +145,7 @@ public class Patterns {
         int t = scan.nextInt(); // take no. of test cases
         for (int i = 0; i < t; i++) {
             int n = scan.nextInt();
-            pattern4(n);
+            pattern12(n);
         }
         scan.close();
     }
