120859511197f0485ef7e6d0687e707a31b4f705
Mayank <mayankchaurasiya66@gmail.com>
2025-11-23 23:00:05 +0530

    Added Character Hashing

---
+++ DSA-JAVA/CharHashing.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class CharHashing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        // precompute
+        int[] hash = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            hash[s.charAt(i)]++;
+        }
+
+        int q = sc.nextInt();
+        while (q-- > 0) {
+            char c = sc.next().charAt(0);
+            System.out.println(hash[c]);
+        }
+
+        sc.close();
+    }
+}
---
+++ DSA-JAVA/CharacterHashing.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class CharacterHashing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        // converts the uppercase one to lower case and store them in new string.. and
+        // perform precompute on the new String.
+        String newS = s.toLowerCase();
+
+        // precompute
+        int[] hash = new int[26];
+        // int[] hash = new int[256];
+        for (int i = 0; i < newS.length(); i++) {
+            hash[newS.charAt(i) - 'a']++;
+            // hash[s.charAt(i)]++;
+        }
+
+        int q = sc.nextInt();
+        while (q-- != 0) {
+            char c;
+            c = sc.next().charAt(0);
+            System.out.println(hash[c - 'a']);
+        }
+        sc.close();
+    }
+}
