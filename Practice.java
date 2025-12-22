7213599e8808f07a461c0f68901900696fb468f6
Mayank <mayankchaurasiya66@gmail.com>
2025-12-22 22:33:35 +0530

    updated few files

--- Practice.java
+++ Practice.java
@@ -1,5 +1,24 @@
 class Practice {
+    public static void isPalindrome() {
+        int number = 765;
+        int copy = number;
+        int rev = 0;
+
+        while (number > 0) {
+            int rem = number % 10;
+            rev = rev * 10 + rem;
+            number /= 10;
+        }
+
+        if (copy == rev) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello world");
+        isPalindrome();
     }
 }
