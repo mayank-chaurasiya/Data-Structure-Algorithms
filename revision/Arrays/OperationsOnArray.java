50bac61c4fb531a751f0415583c85f2b0741c5e0
Mayank <mayankchaurasiya66@gmail.com>
2025-12-25 00:11:43 +0530

    Added functions to find Subarrays

--- revision/Arrays/OperationsOnArray.java
+++ revision/Arrays/OperationsOnArray.java
@@ -77,10 +77,55 @@ public class OperationsOnArray {
 
     // reverseArray(arr);
 
+    public static void pairsInArr(int arr[]) {
+        int first = 0, last = arr.length - 1;
+        for (int i = first; i <= last; i++) {
+            for (int j = i + 1; j <= last; j++) {
+                System.out.print("(" + arr[i] + "," + arr[j] + ") ");
+            }
+            System.out.println();
+        }
+    }
+
+    // pairsInArr(arr);
+
+    public static void printSubarray(int arr[]) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                int sum = 0;
+                StringBuilder sb = new StringBuilder();
+
+                for (int k = i; k <= j; k++) {
+                    sb.append(arr[k] + " ");
+                    sum += arr[k];
+                }
+                System.out.printf("%-15s sum is : %d%n", sb.toString(), sum);
+                // System.out.println();
+                // System.out.print(" sum is : " + sum);
+                // System.out.println();
+
+                if (sum > max) {
+                    max = sum;
+                }
+                if (sum < min) {
+                    min = sum;
+                }
+            }
+            System.out.println();
+        }
+        System.out.print("Minimum is : " + min + "  ");
+        System.out.print("Maximum is : " + max);
+
+    }
+
+    // printSubarray(arr);
+
+    
     public static void main(String[] args) {
         // create
         int arr[] = { 2, 5, 8, 10, 30 };
-        reverseArray(arr);
         // Scanner sc = new Scanner(System.in);
 
         // sc.close();
