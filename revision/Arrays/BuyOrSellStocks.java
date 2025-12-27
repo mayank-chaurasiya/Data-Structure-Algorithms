d08971ba062474e45b3620df669bc0cb6f71fdf5
Mayank <mayankchaurasiya66@gmail.com>
2025-12-27 23:40:42 +0530

    added function to buy Or sell stock

--- revision/Arrays/BuyOrSellStocks.java
+++ revision/Arrays/BuyOrSellStocks.java
@@ -1,44 +1,24 @@
 public class BuyOrSellStocks {
 
-    public static int buySellStocks(int prices[]) {
-        int buyStock = Integer.MAX_VALUE;
-        int sellStock = Integer.MIN_VALUE;
+    public static int buyAndSellStocks(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
 
         for (int i = 0; i < prices.length; i++) {
-
-            // check for the lowest price and assign it to buyStock
-            buyStock = Math.min(prices[i], buyStock);
-
-            // if (prices[i] < buyStock) {
-            // buyStock = prices[i];
-            // }
-
-            // check for the high price and assign it to sellStock
-            sellStock = Math.max(prices[i], sellStock);
-            
-            // if (prices[i] > sellStock) {
-            // sellStock = prices[i];
-            // }
-
+            if (buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i];
+            }
         }
 
-        if (buyStock < sellStock) {
-            return sellStock - buyStock;
-        }
-
-        return 0;
+        return maxProfit;
     }
 
     public static void main(String[] args) {
         int prices[] = { 7, 1, 5, 3, 6, 4 };
 
-        int profit = buySellStocks(prices);
-
-        if (profit == 0) {
-            System.out.println("No profit achieved");
-        } else {
-            System.out.println("The profit is : " + buySellStocks(prices));
-        }
-
+        System.out.println(buyAndSellStocks(prices));
     }
 }
