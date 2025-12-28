df8b20253aa9ce6b250647700dacdeed40370ade
Mayank <mayankchaurasiya66@gmail.com>
2025-12-29 00:47:13 +0530

    Added BubbleSort Sorting technique

---
+++ revision/Sorting/SortingAlgorithms.java
@@ -0,0 +1,30 @@
+package revision.Sorting;
+
+public class SortingAlgorithms {
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void bubbleSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        printArr(arr);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 3, 1, 2 };
+        bubbleSort(arr);
+    }
+}
