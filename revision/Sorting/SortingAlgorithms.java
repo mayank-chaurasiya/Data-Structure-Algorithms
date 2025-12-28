97d9f976ca1d0e82bfc77a2c965acb6f8fae943e
Mayank <mayankchaurasiya66@gmail.com>
2025-12-29 01:22:27 +0530

    Added Selection Sort Sorting technique

--- revision/Sorting/SortingAlgorithms.java
+++ revision/Sorting/SortingAlgorithms.java
@@ -19,12 +19,25 @@ public class SortingAlgorithms {
                 }
             }
         }
+    }
 
+    public static void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
         printArr(arr);
     }
 
     public static void main(String[] args) {
         int arr[] = { 5, 4, 3, 1, 2 };
-        bubbleSort(arr);
     }
 }
