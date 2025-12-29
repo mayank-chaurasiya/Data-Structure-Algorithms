2cc11e89c0a07c12270880ecefa08ff3975420cf
Mayank <mayankchaurasiya66@gmail.com>
2025-12-29 19:00:56 +0530

    Added function for Insertion Sort

--- revision/Sorting/SortingAlgorithms.java
+++ revision/Sorting/SortingAlgorithms.java
@@ -37,7 +37,25 @@ public class SortingAlgorithms {
         printArr(arr);
     }
 
+    public static void insertionSort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+
+            // finding correct position to insert
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = curr;
+        }
+        printArr(arr);
+    }
+
     public static void main(String[] args) {
         int arr[] = { 5, 4, 3, 1, 2 };
+        insertionSort(arr);
     }
 }
