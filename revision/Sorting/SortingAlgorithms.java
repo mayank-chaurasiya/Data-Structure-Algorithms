0a4bd1afed1f68cfd6cc9de2d061be9190fc7219
Mayank <mayankchaurasiya66@gmail.com>
2025-12-29 20:33:16 +0530

    Added function for counting Sort

--- revision/Sorting/SortingAlgorithms.java
+++ revision/Sorting/SortingAlgorithms.java
@@ -54,8 +54,32 @@ public class SortingAlgorithms {
         printArr(arr);
     }
 
+    public static void countingSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int count[] = new int[largest + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        // sorting
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int arr[] = { 5, 4, 3, 1, 2 };
-        insertionSort(arr);
+        countingSort(arr);
+        printArr(arr);
     }
 }
