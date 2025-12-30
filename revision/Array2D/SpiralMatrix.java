f5f4fadecd12f32737d45fc84d7510a4e7a84212
Mayank <mayankchaurasiya66@gmail.com>
2025-12-31 00:30:13 +0530

    added function to print spiral matrix

--- revision/Array2D/SpiralMatrix.java
+++ revision/Array2D/SpiralMatrix.java
@@ -4,19 +4,29 @@ public class SpiralMatrix {
         int startRow = 0, startCol = 0, endRow = matrix.length - 1, endCol = matrix[0].length - 1;
 
         while (startRow <= endRow && startCol <= endCol) {
+            // top
             for (int j = startCol; j <= endCol; j++) {
                 System.out.print(matrix[startRow][j] + " ");
             }
 
+            // right
             for (int i = startRow + 1; i <= endRow; i++) {
                 System.out.print(matrix[i][endCol] + " ");
             }
 
-            for (int j = endCol - 1; j >= startRow; j--) {
+            // bottom
+            for (int j = endCol - 1; j >= startCol; j--) {
+                if (startRow == endRow) {
+                    break;
+                }
                 System.out.print(matrix[endRow][j] + " ");
             }
 
+            // left
             for (int i = endRow - 1; i >= startRow + 1; i--) {
+                if (startCol == endCol) {
+                    break;
+                }
                 System.out.print(matrix[i][startCol] + " ");
             }
             startRow++;
