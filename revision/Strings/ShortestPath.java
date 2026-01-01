64b92cd064f5015fc08743fef0215a83c3b4a28a
Mayank <mayankchaurasiya66@gmail.com>
2026-01-01 21:43:39 +0530

    Function to calculate shortest route

---
+++ revision/Strings/ShortestPath.java
@@ -0,0 +1,29 @@
+package revision.Strings;
+
+public class ShortestPath {
+    public static double getShortestPath(String str) {
+        int x = 0, y = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'E') {
+                x++;
+            } else if (str.charAt(i) == 'W') {
+                x--;
+            } else if (str.charAt(i) == 'N') {
+                y++;
+            } else {
+                y--;
+            }
+        }
+
+        double shortestPath = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
+        return shortestPath;
+    }
+
+    public static void main(String[] args) {
+        String str = "WNEENESENNN";
+
+        System.out.println(getShortestPath(str));
+    }
+}
