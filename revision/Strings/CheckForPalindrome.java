b2acd5428b2be6f6ac52e36eab7ee48be6175ace
Mayank <mayankchaurasiya66@gmail.com>
2026-01-01 20:54:25 +0530

    Function to check for palindrome of String

---
+++ revision/Strings/CheckForPalindrome.java
@@ -0,0 +1,26 @@
+package revision.Strings;
+
+public class CheckForPalindrome {
+    public static boolean isPalindrome(String s) {
+
+        String reverse = "";
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reverse += s.charAt(i);
+        }
+
+        if (s.equals(reverse)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s = "raceAcar";
+
+        s = s.toLowerCase();
+
+        System.out.println(isPalindrome(s));
+    }
+}
