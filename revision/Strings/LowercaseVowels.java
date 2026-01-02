005388f2d1828d168cd397438328c087fb69344c
Mayank <mayankchaurasiya66@gmail.com>
2026-01-02 22:23:56 +0530

    Function to count Lower Case vowels

---
+++ revision/Strings/LowercaseVowels.java
@@ -0,0 +1,22 @@
+package revision.Strings;
+
+public class LowercaseVowels {
+    public static int countLowercaseVowels(String str) {
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'o' || str.charAt(i) == 'i'
+                    || str.charAt(i) == 'u') {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String str = "ApnaCollege";
+
+        System.out.println(countLowercaseVowels(str));
+    }
+}
