00d3c97f8fa939d0767df3308bf5eabb4a6beb67
Mayank <mayankchaurasiya66@gmail.com>
2026-01-07 19:40:14 +0530

    OOPs concept

--- revision/Oops/OOPS.java
+++ revision/Oops/OOPS.java
@@ -2,6 +2,22 @@ package revision.Oops;
 
 public class OOPS {
     public static void main(String[] args) {
+        Horse h = new Horse();
+        System.out.println(h.color);
+    }
+}
 
+class Animal {
+    String color;
+
+    Animal() {
+        System.out.println("Animal Constructor is called");
+    }
+}
+
+class Horse extends Animal {
+    Horse() {
+        super.color = "Brown";
+        System.out.println("Horse consturctor is called");
     }
 }
