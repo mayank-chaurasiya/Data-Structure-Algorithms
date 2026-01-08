17a279851bd5e53477816fa6e6634abb1f84cd18
Mayank <mayankchaurasiya66@gmail.com>
2026-01-08 16:45:01 +0530

    Function to print decreasing Number

---
+++ revision/Recursion/DecreasingNumbers.java
@@ -0,0 +1,16 @@
+package revision.Recursion;
+
+public class DecreasingNumbers {
+
+    public static int decreasingNums(int n) {
+        System.out.print(n + " ");
+        if (n == 1) {
+            return 1;
+        }
+        return decreasingNums(n - 1);
+    }
+
+    public static void main(String[] args) {
+        decreasingNums(10);
+    }
+}
