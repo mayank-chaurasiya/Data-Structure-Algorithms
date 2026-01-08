bb8593d7f5a40cd9f74c89ebc1ea5630faedb8f1
Mayank <mayankchaurasiya66@gmail.com>
2026-01-08 19:34:28 +0530

    Function to check if array is sorted or not?

---
+++ revision/Recursion/CheckForSortedArr.java
@@ -0,0 +1,24 @@
+package revision.Recursion;
+
+public class CheckForSortedArr {
+    public static boolean sorted(int[] arr, int i) {
+
+        if (i == arr.length - 1) {
+            return true;
+        }
+        
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+
+        return sorted(arr, i + 1);
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        int i = 0;
+
+        System.out.println(sorted(arr, i));
+    }
+}
