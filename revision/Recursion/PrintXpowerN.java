7f03bfa01ef7152428a8d2e78895af8737c512f3
Mayank <mayankchaurasiya66@gmail.com>
2026-01-10 00:23:41 +0530

    updated pow function with optimized approach

--- revision/Recursion/PrintXpowerN.java
+++ revision/Recursion/PrintXpowerN.java
@@ -2,13 +2,30 @@ package revision.Recursion;
 
 public class PrintXpowerN {
     public static int pow(int x, int n) {
-        if(n == 0) {
+        if (n == 0) {
             return 1;
         }
 
         return x * pow(x, n - 1);
     }
+
+    // Optimized
+    public static int power(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int p = power(x, n / 2);
+        int halfPowerSqre = p * p;
+
+        if (n % 2 != 0) {
+            halfPowerSqre = x * halfPowerSqre;
+        }
+
+        return halfPowerSqre;
+    }
+
     public static void main(String[] args) {
-        System.out.println(pow(2, 10));
+        // System.out.println(pow(2, 10));
+        System.out.println(power(2, 10));
     }
 }
