359a010e27ed1da5736fefe34aa0ab4a0d78fd26
Mayank <mayankchaurasiya66@gmail.com>
2026-01-09 21:33:32 +0530

    Function to check the last occurence

---
+++ revision/Recursion/LastOccurence.java
@@ -0,0 +1,23 @@
+package revision.Recursion;
+
+public class LastOccurence {
+    public static int lastOcc(int[] arr, int i, int key) {
+        if (i == arr.length) {
+            return -1;
+        }
+        int isFound = lastOcc(arr, i + 1, key);
+
+        if (isFound == -1 && arr[i] == key) {
+            return i;
+        }
+
+        return isFound;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 1, 2, 3, 2, 2, 5, 6, 8 };
+
+        System.out.println(lastOcc(arr, 0, 2));
+
+    }
+}
