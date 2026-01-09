bf2aee71ba8002c28717acd2e441e99040b057c0
Mayank <mayankchaurasiya66@gmail.com>
2026-01-09 23:58:22 +0530

    updated pow function

--- revision/Recursion/PrintXpowerN.java
+++ revision/Recursion/PrintXpowerN.java
@@ -2,8 +2,8 @@ package revision.Recursion;
 
 public class PrintXpowerN {
     public static int pow(int x, int n) {
-        if(n == 1) {
-            return x;
+        if(n == 0) {
+            return 1;
         }
 
         return x * pow(x, n - 1);
