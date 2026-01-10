61ec8d4cab69dd428f431856995991c08e75edee
Mayank <mayankchaurasiya66@gmail.com>
2026-01-10 23:45:51 +0530

    Function to find ways to pair friends

---
+++ revision/Recursion/FriendsPairingProblem.java
@@ -0,0 +1,11 @@
+package revision.Recursion;
+
+public class FriendsPairingProblem {
+    public static int countPairings(int n) {
+        return n <= 2 ? n : countPairings(n - 1) + (n - 1) * countPairings(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countPairings(4));
+    }
+}
