9f1bebd903bc70e9410ca169f4bdc7e1e00071b8
Mayank <mayankchaurasiya66@gmail.com>
2026-01-11 15:44:17 +0530

    Funciton to Print String

---
+++ revision/Recursion/BinaryStringsProblem.java
@@ -0,0 +1,21 @@
+package revision.Recursion;
+
+public class BinaryStringsProblem {
+    public static void printStr(int n, String newStr, int lastBit) {
+        if (n == 0) {
+            System.out.println(newStr);
+            return;
+        }
+
+        printStr(n - 1, newStr + "0", 0);
+
+        if (lastBit == 0) {
+            printStr(n - 1, newStr + "1", 1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        printStr(5, "", 0);
+    }
+}
