19ec0276c06cd8bb916f18dd74c6df6915626a79
Mayank <mayankchaurasiya66@gmail.com>
2026-01-12 21:58:21 +0530

    Updated Function to Convert Number into String

--- revision/Recursion/ConvertIntoString.java
+++ revision/Recursion/ConvertIntoString.java
@@ -1,22 +1,34 @@
 package revision.Recursion;
 
 public class ConvertIntoString {
+
+    static String[] digits = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+
     public static void convertNumToString(String str, int idx, StringBuilder s) {
         if (idx == str.length()) {
             System.out.print(s);
             return;
         }
 
-        String[] arr = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
-
         int i = str.charAt(idx) - '0';
 
-        convertNumToString(str, idx + 1, s.append(arr[i] + " "));
+        convertNumToString(str, idx + 1, s.append(digits[i] + " "));
+    }
+
+    public static void printDigits(int number) {
+        if (number == 0) {
+            return;
+        }
+
+        int lastDigit = number % 10;
+        printDigits(number / 10);
+        System.out.print(digits[lastDigit] + " ");
     }
 
     public static void main(String[] args) {
-        String str = "2019";
+        // String str = "2019";
+        // convertNumToString(str, 0, new StringBuilder(""), digits);
 
-        convertNumToString(str, 0, new StringBuilder(""));
+        printDigits(1234);
     }
 }
