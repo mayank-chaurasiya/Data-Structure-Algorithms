82049ad3edfb61e5ed36e2fb586f25f11e53e545
Mayank <mayankchaurasiya66@gmail.com>
2026-01-17 00:52:34 +0530

    Added function for quickSort

--- revision/Sorting/SortingAlgorithms.java
+++ revision/Sorting/SortingAlgorithms.java
@@ -119,9 +119,40 @@ public class SortingAlgorithms {
         }
     }
 
+    public static void quickSort(int arr[], int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+
+        // last element
+        int pivIndex = partition(arr, si, ei);
+        quickSort(arr, si, pivIndex - 1); // left part
+        quickSort(arr, pivIndex + 1, ei);
+    }
+
+    public static int partition(int arr[], int si, int ei) {
+        int pivot = arr[ei];
+        int i = si - 1; // to make place for elements smaller than pivot;
+
+        for (int j = si; j < ei; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
+
     public static void main(String[] args) {
         int arr[] = { 5, 4, 3, 1, 2 };
-        mergeSort(arr, 0, arr.length - 1);
+        quickSort(arr, 0, arr.length - 1);
         printArr(arr);
     }
 }
