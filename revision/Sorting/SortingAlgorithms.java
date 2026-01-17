76543680bdf3e474e00cb1a153788e5a9ee296b0
Mayank <mayankchaurasiya66@gmail.com>
2026-01-17 13:12:29 +0530

    Function to search in Rotated Sorted array

--- revision/Sorting/SortingAlgorithms.java
+++ revision/Sorting/SortingAlgorithms.java
@@ -150,9 +150,39 @@ public class SortingAlgorithms {
         return i;
     }
 
+    public static int search(int arr[], int target, int si, int ei) {
+        if (si > ei) {
+            return -1;
+        }
+
+        int mid = si + (ei - si) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        if (arr[si] <= arr[mid]) {
+            // case a : left
+            if (arr[si] <= target && target <= arr[mid]) {
+                return search(arr, target, si, mid - 1);
+            } else {
+                // case b : right
+                return search(arr, target, mid + 1, ei);
+            }
+        } else {
+            // case c : right
+            if (arr[mid] <= target && target <= arr[ei]) {
+                return search(arr, target, mid + 1, ei);
+            } else {
+                // case d : left
+                return search(arr, target, si, mid - 1);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        int arr[] = { 5, 4, 3, 1, 2 };
-        quickSort(arr, 0, arr.length - 1);
-        printArr(arr);
+        int arr[] = { 4, 5, 6, 7, 0, 1, 2 };
+        int targetIndex = search(arr, 2, 0, arr.length - 1);
+        System.out.print(targetIndex);
     }
 }
