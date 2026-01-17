e3706dbdff66e44799637ffd6a03196130c105c1
Mayank <mayankchaurasiya66@gmail.com>
2026-01-17 16:35:28 +0530

    Functio to get majority element using mergeSort

--- revision/DivideAndConquer/MajorityELements.java
+++ revision/DivideAndConquer/MajorityELements.java
@@ -17,9 +17,42 @@ public class MajorityELements {
         return -1;
     }
 
+    public static int countInRange(int[] arr, int num, int si, int ei) {
+        int count = 0;
+        for (int i = si; i <= ei; i++) {
+            if (arr[i] == num) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int majorityElementRec(int[] arr, int si, int ei) {
+        if (si == ei) {
+            return arr[si];
+        }
+
+        int mid = (ei - si) / 2 + si;
+        int left = majorityElementRec(arr, si, mid);
+        int right = majorityElementRec(arr, mid + 1, ei);
+
+        if (left == right) {
+            return left;
+        }
+
+        int leftCount = countInRange(arr, left, si, ei);
+        int rightCount = countInRange(arr, right, si, ei);
+
+        return leftCount > rightCount ? left : right;
+    }
+
+    public static int majorElement(int[] arr){
+        return majorityElementRec(arr, 0, arr.length - 1);
+    }
+
     public static void main(String[] args) {
         int[] arr = { 2, 2, 1, 1, 1, 2, 2 };
 
-        System.out.print(majorityElement(arr));
+        System.out.print(majorElement(arr));
     }
 }
