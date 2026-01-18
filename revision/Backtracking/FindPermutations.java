c1d0d110719a677f5b2cc355aaa9c154cb698c6d
Mayank <mayankchaurasiya66@gmail.com>
2026-01-19 01:12:48 +0530

    function to find arrangements(permutations) of string

---
+++ revision/Backtracking/FindPermutations.java
@@ -0,0 +1,25 @@
+package revision.Backtracking;
+
+public class FindPermutations {
+    public static void getPermutations(String str, String ans) {
+        if (str.length() == 0) {
+            System.out.print(ans + " ");
+            return;
+        }
+
+        for (int j = 0; j < str.length(); j++) {
+            char ch = str.charAt(j);
+
+            String remaining = str.substring(0, j) + str.substring(j + 1);
+
+            getPermutations(remaining, ans + ch);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+
+        getPermutations(str, "");
+    }
+}
