d008d72fc1ec30f9f790d23e488e1d0a989abe61
Mayank <mayankchaurasiya66@gmail.com>
2026-01-20 16:04:41 +0530

    Function to get ways to reach destination (GRID WAYS)

---
+++ revision/Backtracking/GridWays.java
@@ -0,0 +1,18 @@
+// number of ways to reach from (0, 0) to (n - 1, m - 1) in a N * M Grid. ALLOWED moves - right or down
+
+public class GridWays {
+    public static int nWays(int grid[][], int row, int col) {
+        if (row == grid.length - 1 || col == grid[0].length - 1) {
+            return 1;
+        } else if (row == grid.length || col == grid[0].length) {
+            return 0;
+        }
+        return nWays(grid, row, col + 1) + nWays(grid, row + 1, col);
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = new int[3][3];
+
+        System.out.print(nWays(grid, 0, 0));
+    }
+}
