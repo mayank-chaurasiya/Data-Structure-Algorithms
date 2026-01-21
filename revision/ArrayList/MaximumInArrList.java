5ab0f39b1576f34105fc09760923f0b3d8960504
Mayank <mayankchaurasiya66@gmail.com>
2026-01-21 17:49:06 +0530

    Get the maximum in ArrayList

---
+++ revision/ArrayList/MaximumInArrList.java
@@ -0,0 +1,27 @@
+package revision.ArrayList;
+
+import java.util.ArrayList;
+
+public class MaximumInArrList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+
+        System.out.println(list);
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > max) {
+                max = list.get(i);
+            }
+        }
+
+        System.out.println("Maximum is " + max);
+    }
+}
