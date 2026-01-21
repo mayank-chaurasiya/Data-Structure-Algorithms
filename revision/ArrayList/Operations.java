e5c53250675494a1b4051bbb842aae7535908482
Mayank <mayankchaurasiya66@gmail.com>
2026-01-21 17:45:39 +0530

    Operations on Array List

---
+++ revision/ArrayList/Operations.java
@@ -0,0 +1,23 @@
+package revision.ArrayList;
+
+import java.util.ArrayList;
+
+public class Operations {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(8);
+        list.add(6);
+        list.add(3);
+
+        System.out.println(list);
+        System.out.println(list.contains(6));
+
+        list.remove(2);
+        list.set(2, 78);
+        System.out.println(list);
+
+        System.out.println(list.size());
+    }
+}
