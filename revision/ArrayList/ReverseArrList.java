9f38c12643649240934b9a6ffe98475b89df3e63
Mayank <mayankchaurasiya66@gmail.com>
2026-01-21 17:46:12 +0530

    Reverse the ArrayList

---
+++ revision/ArrayList/ReverseArrList.java
@@ -0,0 +1,27 @@
+package revision.ArrayList;
+
+import java.util.ArrayList;
+
+public class ReverseArrList {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= 5; i++) {
+            list.add(i);
+        }
+
+        System.out.println(list);
+        System.out.println(list.reversed());
+
+        ArrayList<Integer> temp = new ArrayList<>();
+        for (int i = list.size(); i > 0; i--) {
+            temp.add(i);
+        }
+
+        System.out.println(temp);
+
+
+    }
+
+}
