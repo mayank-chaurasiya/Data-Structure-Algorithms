3fa2f5024e158a94de50b5b36204ce62575c5d16
Mayank <mayankchaurasiya66@gmail.com>
2026-01-22 17:52:18 +0530

    Concatenation of array

---
+++ LeetCode/Quest/Array/ConcatenationOfArray.java
@@ -0,0 +1,24 @@
+package LeetCode.Quest.Array;
+
+class ConcatenationOfArray {
+    public static int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[2 * n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 1 };
+
+        int result[] = getConcatenation(nums);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+}
