1e8c213f9ca6bdf0c3453cfe8a0e2f5f08424908
Mayank <mayankchaurasiya66@gmail.com>
2026-01-22 22:47:10 +0530

    Monotonic array

---
+++ revision/ArrayList/MonotonicArrList.java
@@ -0,0 +1,33 @@
+package revision.ArrayList;
+
+import java.util.ArrayList;
+
+public class MonotonicArrList {
+    public static boolean isMonotonic(ArrayList<Integer> list) {
+        boolean increasing = true;
+        boolean decreasing = true;
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) > list.get(i + 1)) {
+                increasing = false;
+            }
+            if (list.get(i) < list.get(i + 1)) {
+                decreasing = false;
+            }
+        }
+
+        return increasing || decreasing;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        System.out.println(isMonotonic(list));
+    }
+}
