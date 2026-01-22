92f15defd654672b1a73d29690aa4ee2a11034ed
Mayank <mayankchaurasiya66@gmail.com>
2026-01-22 18:00:55 +0530

    updated things

---
+++ LeetCode/Problems/Q07_ReverseInteger.java
@@ -0,0 +1,20 @@
+package LeetCode.Problems;
+
+public class Q07_ReverseInteger {
+    public static int reverse(int x) {
+        int revNum = 0;
+        while (x != 0) {
+            int lastDigit = x % 10;
+            if (revNum > Integer.MAX_VALUE / 10 || revNum < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+            revNum = (revNum * 10) + lastDigit;
+            x = x / 10;
+        }
+        return revNum;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(reverse(76543));
+    }
+}
