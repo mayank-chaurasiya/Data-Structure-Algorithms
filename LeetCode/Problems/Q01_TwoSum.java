20bf8b63c86c7e6c9387fd7fbbe9116efce83a0a
Mayank <mayankchaurasiya66@gmail.com>
2026-01-22 18:48:35 +0530

    Two sum

---
+++ LeetCode/Problems/Q01_TwoSum.java
@@ -0,0 +1,16 @@
+package LeetCode.Problems;
+
+public class Q01_TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+
+        // if no valid pair is found..
+        return new int[] {};
+    }
+}
