509c4db0de558104cba2f01b95a5467a296dbd71
Mayank <mayankchaurasiya66@gmail.com>
2026-01-23 16:53:15 +0530

    Functio to add in first, middle and last

---
+++ revision/LinkedList/LinkedList.java
@@ -0,0 +1,78 @@
+package revision.LinkedList;
+
+public class LinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    // Methods to add()
+    public void addFirst(int data) {
+        Node newNode = new Node(data);// step 1 - create new node
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;// step 2 - newNode next = head
+        head = newNode; // step 3 - head = newNode
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void addInMiddle(int index, int data) {
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Methods to print()
+    public void printLL() {
+        if (head == null) {
+            System.out.println("NULL");
+            return;
+        }
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+
+    // Methods to remove()
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.printLL();
+        ll.addFirst(2);
+        ll.printLL();
+        ll.addFirst(1);
+        ll.printLL();
+        ll.addLast(3);
+        ll.printLL();
+        ll.addLast(4);
+        ll.printLL();
+    }
+}
