17a17378283582e4486a3df50233d17c85e05fb9
Mayank <mayankchaurasiya66@gmail.com>
2026-01-25 21:39:03 +0530

    Function to remove nth node from the last in doubly linked list

--- revision/LinkedList/DoublyLL.java
+++ revision/LinkedList/DoublyLL.java
@@ -35,9 +35,47 @@ public class DoublyLL {
             head = tail = newNode;
             return;
         }
-        newNode.next = head;
-        head.prev = newNode;
-        head = newNode;
+        newNode.next = head; // forward link
+        head.prev = newNode; // backward link
+        head = newNode; // update tail
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode; // forward link
+        newNode.prev = tail; // backward link
+        tail = newNode; // update tail
+    }
+
+    public void addInMiddle(int index, int data) {
+        if (index == 0) {
+            addFirst(data);
+            return;
+        }
+
+        if (index == size) {
+            addLast(data);
+            return;
+        }
+
+        Node newNode = new Node(data);
+        Node temp = head;
+
+        int i = 0;
+        while (i < index - 1) {
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        newNode.prev = temp;
+        temp.next.prev = newNode;
+        temp.next = newNode;
+        size++;
     }
 
     // remove
@@ -59,19 +97,87 @@ public class DoublyLL {
         return val;
     }
 
+    public int removeLast() {
+        if (size == 0) {
+            System.out.println("doubly linked list is empty");
+            return Integer.MIN_VALUE;
+        }
+
+        int val = tail.data;
+
+        if (size == 1) {
+            head = tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+        }
+
+        size--;
+        return val;
+    }
+
+    public void removeNthNode(int n) {
+        if (head == null) {
+            return;
+        }
+        int sz = 0;
+        Node temp = head;
+        while (temp != null) {
+            temp = temp.next;
+            sz++;
+        }
+        if (n <= 0 || n > sz) {
+            return;
+        }
+        if (n == sz) {
+            head = head.next;
+            if (head != null) {
+                head.prev = null;
+            } else {
+                tail = null;
+            }
+            size--;
+            return;
+        }
+
+        int i = 1;
+        int iToFind = sz - n + 1;
+        Node prev = head;
+        while (i < iToFind) {
+            prev = prev.next;
+            i++;
+        }
+
+        Node toDelete = prev.next;
+        size--;
+        prev.next = toDelete.next;
+
+        if (toDelete.next != null) {
+            toDelete.next.prev = prev;
+        } else {
+            tail = prev;
+        }
+    }
+
     public static void main(String[] args) {
         DoublyLL dll = new DoublyLL();
 
         dll.addFirst(3);
         dll.addFirst(2);
         dll.addFirst(1);
+        dll.addInMiddle(2, 5);
+        dll.addLast(4);
 
         dll.print();
         System.out.println("size of doubly linked list is : " + size);
 
-        dll.removeFirst();
-
+        dll.removeNthNode(2);
         dll.print();
         System.out.println("size of doubly linked list is : " + size);
+
+        // dll.removeFirst();
+        // dll.removeLast();
+        // dll.print();
+        // System.out.println("size of doubly linked list is : " + size);
     }
 }
