cd06bf56b8a1ab598a8c2a65d82c0aa8f3a9fd27
Mayank <mayankchaurasiya66@gmail.com>
2026-01-27 20:14:26 +0530

    updated stack opearations

---
+++ revision/StackOperations/StackUsingCF.java
@@ -0,0 +1,19 @@
+package revision.StackOperations;
+
+import java.util.*;
+
+public class StackUsingCF {
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(8);
+        s.push(89);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
