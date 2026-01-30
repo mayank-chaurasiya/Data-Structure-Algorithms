45e48c99ba24dbed88b0a7a0875f79d81d58b03a
Mayank <mayankchaurasiya66@gmail.com>
2026-01-30 19:40:51 +0530

    Check for the valid parenthesis

---
+++ revision/StackOperations/ValidParentheses.java
@@ -0,0 +1,36 @@
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String str) {
+        Stack<Character> s = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            // opening 
+            if (ch == '(' || ch == '{' || ch == '[') {
+                s.push(ch);
+            } else {
+                // closing 
+                if (s.isEmpty()) {
+                    return false;
+                }
+                if ((s.peek() == '(' && ch == ')') || (s.peek() == '{' && ch == '}') || (s.peek() == '[' && ch == ']')) {
+                    s.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return  s.isEmpty();
+
+    }
+
+    public static void main(String[] args) {
+        String str = "({})[";
+
+        System.out.print(isValid(str));
+    }
+}
