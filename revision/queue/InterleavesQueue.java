57a22b58d40d25377df637e8a8888b315b1574c2
Mayank <mayankchaurasiya66@gmail.com>
2026-02-04 23:29:39 +0530

    Function to get Interleave of a Queue

---
+++ revision/queue/InterleavesQueue.java
@@ -0,0 +1,35 @@
+package revision.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class InterleavesQueue {
+
+    public static void interLeave(Queue<Integer> q) {
+        Queue<Integer> firstHalf = new LinkedList<>();
+        int size = q.size();
+
+        for (int i = 0; i < size / 2; i++) {
+            firstHalf.add(q.remove());
+        }
+
+        while (!firstHalf.isEmpty()) {
+            q.add(firstHalf.remove());
+            q.add(q.remove());
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= 10; i++) {
+            q.add(i);
+        }
+
+        interLeave(q);
+
+        while (!q.isEmpty()) {
+            System.out.print(q.remove() + " ");
+        }
+    }
+}
