7a0b97b02eeb2a32b30dccba24b7b329e29be222
Mayank <mayankchaurasiya66@gmail.com>
2026-02-06 21:02:25 +0530

    Activity Selection

--- revision/greedyAlgorithm/ActivitySelection.java
+++ revision/greedyAlgorithm/ActivitySelection.java
@@ -1,5 +1,10 @@
 package revision.greedyAlgorithm;
 
+// Activity Selection
+
+// Given two arrays start[] and finish[], representing the start and finish times of activities. A person can perform only one activity at a time, and an activity can be performed only if its start time is greater than the finish time of the last chosen activity.
+// Find the maximum number of activities that can be performed without overlapping.
+
 public class ActivitySelection {
-    
+
 }
