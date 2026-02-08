7520ab9b31798dd23e4a6dbc795ed0b2187b56f9
Mayank <mayankchaurasiya66@gmail.com>
2026-02-08 19:55:01 +0530

    Activity Selection

--- revision/greedyAlgorithm/ActivitySelection.java
+++ revision/greedyAlgorithm/ActivitySelection.java
@@ -1,12 +1,51 @@
-package revision.greedyAlgorithm;
-
 // Activity Selection
 
 // Given two arrays start[] and finish[], representing the start and finish times of activities. A person can perform only one activity at a time, and an activity can be performed only if its start time is greater than the finish time of the last chosen activity.
 // Find the maximum number of activities that can be performed without overlapping.
 
+package revision.greedyAlgorithm;
+
+import java.util.*;
+
 public class ActivitySelection {
     public static void main(String[] args) {
-        
+        int start[] = { 1, 3, 0, 5, 8, 5 };
+        int end[] = { 2, 4, 6, 7, 9, 9 };
+
+        // sorting
+        int activities[][] = new int[start.length][3];
+        for (int i = 0; i < start.length; i++) {
+            activities[i][0] = i;
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
+
+        // lambda function
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
+
+        // end time basis sorted
+        int maxActs = 0;
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        // 1st activity
+        maxActs = 1;
+        ans.add(activities[0][0]);
+
+        int lastEnd = activities[0][2];
+
+        for (int i = 1; i < end.length; i++) {
+            if (activities[i][1] >= lastEnd) {
+                maxActs++;
+                ans.add(activities[i][0]);
+                lastEnd = activities[i][2];
+            }
+        }
+
+        System.out.println("max activities : " + maxActs);
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print("A" + ans.get(i) + " ");
+        }
+
+        System.out.println();
     }
 }
