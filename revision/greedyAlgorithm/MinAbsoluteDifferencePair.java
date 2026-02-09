3c2cf0f2a9625eb5c4be7cf86cb0f757bbc031d3
Mayank <mayankchaurasiya66@gmail.com>
2026-02-10 01:15:23 +0530

    Get Min Absolute difference

---
+++ revision/greedyAlgorithm/MinAbsoluteDifferencePair.java
@@ -0,0 +1,21 @@
+package revision.greedyAlgorithm;
+
+import java.util.*;
+
+public class MinAbsoluteDifferencePair {
+    public static void main(String[] args) {
+        int A[] = { 1, 2, 3 };
+        int B[] = { 2, 1, 3 };
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int minDiff = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            minDiff += Math.abs(A[i] - B[i]);
+        }
+
+        System.out.println("Min absolute diff of pairs = " + minDiff);
+    }
+}
