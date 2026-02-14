ea46bb6e354666de53c8e5948058bf4ad1e03c24
mayank-chaurasiya <try.maynk@gmail.com>
2026-02-14 16:55:38 +0530

    Coin change problem

---
+++ revision/greedyAlgorithm/IndianCoins.java
@@ -0,0 +1,31 @@
+package revision.greedyAlgorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class IndianCoins {
+    public static void main(String[] args) {
+        Integer coins[] = { 1, 2, 5 };
+        Arrays.sort(coins, Comparator.reverseOrder());
+        int amount = 11;
+        int count = 0;
+
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < coins.length; i++) {
+            if (coins[i] <= amount) {
+                while (coins[i] <= amount) {
+                    count++;
+                    ans.add(coins[i]);
+                    amount = amount - coins[i];
+                }
+            }
+        }
+        System.out.println("total minimum coins used is : " + count);
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+
+        }
+    }
+}
