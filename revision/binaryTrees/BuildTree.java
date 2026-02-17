753878baeed0d4cfc5c5e1234ed3a27353396fd6
mayank-chaurasiya <try.maynk@gmail.com>
2026-02-17 21:16:20 +0530

    Tree Traversal - preorder

--- revision/binaryTrees/BuildTree.java
+++ revision/binaryTrees/BuildTree.java
@@ -28,12 +28,22 @@ public class BuildTree {
 
             return newNode;
         }
+
+        public void preorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
     }
 
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+
+        tree.preorder(root);
     }
 }
