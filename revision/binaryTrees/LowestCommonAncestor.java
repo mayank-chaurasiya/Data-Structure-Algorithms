b68a8aa8c2b31cd21493fa16ee1904f33c8ba413
mayank-chaurasiya <try.maynk@gmail.com>
2026-02-19 23:26:30 +0530

    Get Lowest commong ancestor approach 2

--- revision/binaryTrees/LowestCommonAncestor.java
+++ revision/binaryTrees/LowestCommonAncestor.java
@@ -50,6 +50,26 @@ public class LowestCommonAncestor {
         return lca;
     }
 
+    // approach 2
+    public static Node lca2(Node root, int n1, int n2) {
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+
+        if (rightLca == null) {
+            return leftLca;
+        }
+
+        if (leftLca == null) {
+            return rightLca;
+        }
+
+        return root;
+    }
+
     static Node newNode(int data) {
         Node temp = new Node();
         temp.data = data;
@@ -68,7 +88,7 @@ public class LowestCommonAncestor {
 
         int n1 = 4, n2 = 5;
 
-        System.out.println(lca(root, n1, n2).data);
+        System.out.println(lca2(root, n1, n2).data);
 
     }
 }
