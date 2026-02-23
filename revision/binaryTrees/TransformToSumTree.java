47ca8f4bed7c0b8358c947264e8a04f0d00e68d5
mayank-chaurasiya <try.maynk@gmail.com>
2026-02-23 23:10:49 +0530

    Transform a sum tree

---
+++ revision/binaryTrees/TransformToSumTree.java
@@ -0,0 +1,54 @@
+package revision.binaryTrees;
+
+public class TransformToSumTree {
+    static class Node {
+        int data;
+        Node left, right;
+    }
+
+    static Node newNode(int data) {
+        Node temp = new Node();
+        temp.data = data;
+        temp.left = temp.right = null;
+        return temp;
+    }
+
+    public static int transform(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftChild = transform(root.left);
+        int rightChild = transform(root.right);
+
+        int data = root.data;
+
+        int newLeft = root.left == null ? 0 : root.left.data;
+        int newRight = root.right == null ? 0 : root.right.data;
+
+        root.data = newLeft + leftChild + newRight + rightChild;
+        return data;
+    }
+
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node root = newNode(1);
+        root.left = newNode(2);
+        root.right = newNode(3);
+        root.left.left = newNode(4);
+        root.left.right = newNode(5);
+        root.right.left = newNode(6);
+        root.right.right = newNode(7);
+
+        transform(root);
+        preorder(root);
+    }
+}
