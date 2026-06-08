0289c05677d98646fe7ad3f66c780e97fe5d61da
mayank-chaurasiya <try.maynk@gmail.com>
2026-06-08 18:21:07 +0530

    setMismatch solution

---
+++ LeetCode/Quest/arrayII/Q1_SetMismatch.java
@@ -0,0 +1,33 @@
+package leetcode.quest.arrayII;
+
+public class Q1_SetMismatch {
+    public static int[] findErrorNums(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+
+            if (nums[i] != nums[correct]) {
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            } else {
+                i++;
+            }
+        }
+        for (i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return new int[] { nums[i], i + 1 };
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 2, 4 };
+        int[] result = findErrorNums(nums);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+}
