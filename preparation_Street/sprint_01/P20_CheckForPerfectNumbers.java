701bc8be699e06660611f8503c405832027c64c3
mayank-chaurasiya <try.maynk@gmail.com>
2026-06-19 16:25:46 +0530

    updated logic

--- preparation_Street/sprint_01/P20_CheckForPerfectNumbers.java
+++ preparation_Street/sprint_01/P20_CheckForPerfectNumbers.java
@@ -3,13 +3,9 @@ package preparation_street.sprint_01;
 import java.util.Scanner;
 
 public class P20_CheckForPerfectNumbers {
-    public static void main(String[] args) {
-        long n, sum = 0;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the number : ");
-        n = scan.nextLong();
+    public static boolean checkPerfectNumber(long n) {
         int i = 1;
-
+        long sum = 0;
         while (i <= n / 2) {
             if (n % i == 0) {
                 sum = sum + i;
@@ -17,10 +13,18 @@ public class P20_CheckForPerfectNumbers {
             i++;
         }
         if (sum == n) {
-            System.out.println(n + " is a perfect square");
-        } else {
-            System.out.println(n + " is not a perfect square.");
+            System.out.print(n + " ");
+            return true;
         }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        long n;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        n = scan.nextLong();
+        System.out.println(checkPerfectNumber(n));
         scan.close();
     }
 }
